6943fde8e4dd904b24e70adf9fd794f54629e706
Tom Phillips <tomphillipsmusic@gmail.com>
2022-01-23 13:42:37 -0500

    Initial Commit

---
+++ Simon/SimonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonApp.swift
+//  Simon
+//
+//  Created by Tom Phillips on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct SimonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
